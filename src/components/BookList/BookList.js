1ac1278f26aa130c236d12bc1fda0f5000ad5ad0
Kyle King <bkkcreative16@gmail.com>
2021-05-20 18:05:48 -0500

    added error  handling

--- src/components/BookList/BookList.js
+++ src/components/BookList/BookList.js
@@ -2,15 +2,15 @@ import React from "react";
 import Book from "./Book/Book";
 import "./BookList.scss";
 
-const BookList = ({ books }) => {
+const BookList = ({ books, error }) => {
   return (
     <section className="book-list">
-      {books ? (
+      {books || error ? (
         books.map((book) => {
           return <Book key={book.id} book={book.volumeInfo} />;
         })
       ) : (
-        <h1>No books matching your query</h1>
+        <h1 className="no-books">No books matching your query</h1>
       )}
     </section>
   );
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,12 +1,15 @@
 import React, { useState } from "react";
 import BookList from "../../components/BookList/BookList";
 import Search from "../../components/Search/Search";
+import Skeleton from "react-loading-skeleton";
 import axios from "axios";
 import "./Home.scss";
 
 const Home = () => {
   const [search, setSearch] = useState("");
+  const [loading, setLoading] = useState(false);
   const [books, setBooks] = useState([]);
+  const [error, setError] = useState("");
 
   const searchBooks = async (e) => {
     e.preventDefault();
@@ -14,10 +17,13 @@ const Home = () => {
 
     try {
       const apiUrl = `https://www.googleapis.com/books/v1/volumes?q=${search}&key=${process.env.REACT_APP_API_KEY}`;
+
       const { data } = await axios.get(apiUrl);
+      setLoading(true);
+      setLoading(false);
       setBooks(data.items);
     } catch (error) {
-      console.log(error.message);
+      setError(error.message);
     }
   };
 
@@ -26,7 +32,11 @@ const Home = () => {
       <h1 className="home__heading">Book Finder</h1>
       {search.length === 0 && <h3 className="error">Please enter something</h3>}
       <Search search={search} searchBooks={searchBooks} setSearch={setSearch} />
-      <BookList books={books} />
+      {loading ? (
+        <Skeleton height={`100vh`} />
+      ) : (
+        <BookList error={error} books={books} />
+      )}
     </section>
   );
 };
